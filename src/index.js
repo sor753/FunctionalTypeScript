db43dc0e745d8c09648060e65ce61949e5606887
ShowKawazoe <kawazoe.designare@gmail.com>
2024-03-07 05:01:59 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function multiply(a, b) {
+    return a * b;
+}
+var a = 2;
+var b = 3;
+console.log("".concat(a, " * ").concat(b, " = ").concat(multiply(a, b)));
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import _ from "lodash";
+
+_.times(4, () => {
+  console.log("Hello, world!");
+});
